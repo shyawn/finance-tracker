3079f587442c18a65a7d79a6e1eab9c3dd7876ca
ShawnCsj <csjshawn@gmail.com>
2025-02-18 17:03:02 +0800

    feat: setup react router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,17 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <div className="app-container">
+        <Routes>
+          <Route path="/" element={<h1>Dashboard</h1>}></Route>
+          <Route path="/auth" element={<h1>Sign in</h1>}></Route>
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
