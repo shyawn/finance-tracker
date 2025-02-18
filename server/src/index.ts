38ae0708009015a9f95b26a55a3ace1c9b8d3b02
ShawnCsj <csjshawn@gmail.com>
2025-02-18 18:16:14 +0800

    feat: create express script

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,21 @@
-// mongodb+srv://csjshawn:ON9xMag9XMyr8jZu@personalfinancetracker.vwe6y.mongodb.net/
-import express, { Express } from "express";
+// import express, { Express } from "express";
 import mongoose from "mongoose";
 
-const app: Express = express();
+// const app: Express = express();
+const express = require("express");
+const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(express.json());
+
+const mongoURI: string =
+  "mongodb+srv://csjshawn:ON9xMag9XMyr8jZu@personalfinancetracker.vwe6y.mongodb.net/";
+
+mongoose
+  .connect(mongoURI)
+  .then(() => console.log("Connected to mongoDb"))
+  .catch((err) => console.error("Failed to connect to mongoDb: ", err));
+
+app.listen(port, () => {
+  console.log(`Server Running on Port ${port}`);
+});
