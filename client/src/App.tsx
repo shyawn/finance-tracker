9c3ed058ac09db15633cd4eaee7c3d5a1b2fe4e6
ShawnCsj <csjshawn@gmail.com>
2025-02-18 17:12:39 +0800

    feat: add auth page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,15 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Dashboard } from "./pages/dashboard";
+import { Auth } from "./pages/auth";
 
 function App() {
   return (
     <Router>
       <div className="app-container">
         <Routes>
-          <Route path="/" element={<h1>Dashboard</h1>}></Route>
-          <Route path="/auth" element={<h1>Sign in</h1>}></Route>
+          <Route path="/" element={<Dashboard />}></Route>
+          <Route path="/auth" element={<Auth />}></Route>
         </Routes>
       </div>
     </Router>
