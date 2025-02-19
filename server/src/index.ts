c54c8c2f5383cf4140f15a51e25d84664752930f
ShawnCsj <csjshawn@gmail.com>
2025-02-19 14:24:49 +0800

    chore: edit __dirname

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,12 +7,17 @@ const dotenv = require("dotenv");
 const financialRecordRouter = require("./routes/financial-records");
 const cors = require("cors");
 const path = require("path");
+// const url = require("url");
 
 // const app: Express = express();
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
-const _dirname = path.resolve();
+// const __dirname = path.resolve();
+// import { fileURLToPath } from "url";
+
+// const __filename = url.fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 app.use(express.json());
 app.use(cors());
@@ -28,9 +33,9 @@ mongoose
 app.use("/financial-records", financialRecordRouter);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(_dirname, "../client/dist")));
+  app.use(express.static(path.join(__dirname, "../client/dist")));
   app.get("*", (req: any, res: any) => {
-    res.sendFile(path.join(_dirname, "../client", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
   });
 }
 
